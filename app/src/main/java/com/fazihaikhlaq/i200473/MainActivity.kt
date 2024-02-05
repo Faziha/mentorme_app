b6ab620309732a0fc1b7f98432b8d9e67340da79
Faziha Ikhlaq <fazihaikhlaq101@gmail.com>
2024-02-05 14:33:03 +0500

    2nd screen

--- app/src/main/java/com/fazihaikhlaq/i200473/MainActivity.kt
+++ app/src/main/java/com/fazihaikhlaq/i200473/MainActivity.kt
@@ -2,10 +2,21 @@ package com.fazihaikhlaq.i200473
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.style.UnderlineSpan
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var signUptext: TextView
+        signUptext = findViewById(R.id.signUp)
+        var str = "Sign up"
+        val mSpannableString = SpannableString(str)
+
+        mSpannableString.setSpan(UnderlineSpan(), 0, mSpannableString.length, 0)
+
+        signUptext.text = mSpannableString
     }
 }
