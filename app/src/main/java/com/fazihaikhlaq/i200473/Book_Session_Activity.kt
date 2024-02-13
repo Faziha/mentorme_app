e4aa03d1980fde03c021c602eb831c496c2ebde3
Faziha Ikhlaq <fazihaikhlaq101@gmail.com>
2024-02-13 19:20:44 +0500

    11th screen

--- app/src/main/java/com/fazihaikhlaq/i200473/Book_Session_Activity.kt
+++ app/src/main/java/com/fazihaikhlaq/i200473/Book_Session_Activity.kt
@@ -1,11 +1,20 @@
 package com.fazihaikhlaq.i200473
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 
 class Book_Session_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_session)
+
+        findViewById<ImageView>(R.id.imageView24).setOnClickListener{
+            startActivity(Intent(this,Feedback_Activity::class.java))
+        }
+
     }
+
 }
